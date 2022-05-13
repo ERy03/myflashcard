417b8ba552565f50cfb6bb8b3a1d2db568478db6
Ryota <ryoenoki2013@gmail.com>
2022-05-13 14:57:10 +0900

    creating new class for button

---
+++ lib/components/button_with_icon.dart
@@ -0,0 +1,8 @@
+class ButtonWithIcon extends StatelessWidget {
+  const ButtonWithIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
