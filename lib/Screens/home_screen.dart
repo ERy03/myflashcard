9f0fc3f328d64948a391fe1ce022b46667f70d57
Ryota <ryoenoki2013@gmail.com>
2022-05-13 11:02:42 +0900

    home screen text and image added

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -10,10 +10,37 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Image.asset("assets/images/image_title.png"),
+            ),
+            _title_text(),
+            // TODO 横線、
+            // TODO かくにんテストをするボタン、
+            // TODO radio button、
+            // TODO 単語一覧を表示するボタン、
+            Text("Powered by Ryota @ Dreamly 2022"),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _title_text() {
+    return Column(
+      children: <Widget>[
+        Text(
+          "私だけの単語帳",
+          style: TextStyle(fontSize: 40.0),
+        ),
+        Text(
+          "My Own Flashcard",
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ],
     );
   }
 }
